c5f06ae035d8513361d0af24bbcffa606feb3fdf
github username <github email address>
2018-09-17 16:00:11 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,41 @@
+import uuid from "uuid";
+
 export default (state = [], action) => {
-  return state;
+
+  switch (action.type) {
+    case "ADD_QUOTE" :
+      return [...state, action.quote]
+    case "REMOVE_QUOTE" :
+      let quote = state.filter(quote => quote.id === action.quoteId)
+      let idx = state.indexOf(quote)
+      if (idx > -1) {
+        state.splice(idx, 1)
+      } else if (idx === -1) {
+        state.shift()
+      }
+      return state
+    case "UPVOTE_QUOTE" :
+      let newState = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          quote.votes = quote.votes + 1
+          return quote
+        } else {
+          return quote
+        }
+      })
+      return newState
+    case "DOWNVOTE_QUOTE" :
+      let arr = state.map(quote => {
+        if (quote.id === action.quoteId && quote.votes !== 0) {
+          quote.votes = quote.votes - 1
+          return quote
+        } else {
+          return quote
+        }
+      })
+      return arr
+    default :
+      return state
+
+  }
 }
